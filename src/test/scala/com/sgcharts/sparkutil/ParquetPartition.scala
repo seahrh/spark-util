d23caf0b8ece79df8881afb39aae3290099f5c8a
Ruhong <seahrh@gmail.com>
2018-02-04 10:03:25 +0800

    use inheritance to structure daily and daily-country partitions

--- src/test/scala/com/sgcharts/sparkutil/ParquetPartition.scala
+++ src/test/scala/com/sgcharts/sparkutil/ParquetPartition.scala
@@ -2,15 +2,10 @@ package com.sgcharts.sparkutil
 
 import org.apache.spark.sql.{Dataset, SaveMode, SparkSession}
 
-trait ParquetPartition[T] extends UnmanagedTablePartition[T]
+sealed trait ParquetPartition[T] extends UnmanagedTablePartition[T]
 
-final case class DailyPartition[T](
-                              override val spark: SparkSession,
-                              override val table: String,
-                              override val tablePath: String,
-                              override val ds: Dataset[T],
-                              date: String
-                            ) extends ParquetPartition[T] {
+sealed trait TDailyPartition[T] extends ParquetPartition[T] {
+  val date: String
 
   override def spec: String = s"""date="$date""""
 
@@ -23,6 +18,38 @@ final case class DailyPartition[T](
 
   override def append(): Unit = {
     writer(SaveMode.Append).partitionBy("date").parquet(tablePath)
+    addPartition()
+  }
+}
+
+final case class DailyPartition[T](
+                              override val spark: SparkSession,
+                              override val table: String,
+                              override val tablePath: String,
+                              override val ds: Dataset[T],
+                              override val date: String
+                            ) extends TDailyPartition[T]
+
+final case class DailyCountryPartition[T](
+                                    override val spark: SparkSession,
+                                    override val table: String,
+                                    override val tablePath: String,
+                                    override val ds: Dataset[T],
+                                    override val date: String,
+                                    country: String
+                                  ) extends TDailyPartition[T] {
+
+  override def spec: String = s"""${super.spec},country="$country""""
+
+  override def path: String = s"${super.path}/country=$country"
+
+  override def overwrite(): Unit = {
+    writer(SaveMode.Overwrite).partitionBy("date", "country").parquet(tablePath)
+    addPartition()
+  }
+
+  override def append(): Unit = {
+    writer(SaveMode.Append).partitionBy("date", "country").parquet(tablePath)
     addPartition()
   }
 }
