18c9b4b413253a48d6f17b350832bf518d041ae1
seahrh <seahrh@gmail.com>
2019-04-23 17:11:48 +0800

    move credentials to build.sbt

--- .ci/publish.sh
+++ .ci/publish.sh
@@ -5,7 +5,7 @@ set -o verbose
 
 echo "Setup gpg keys"
 
-gpg --keyserver keyserver.ubuntu.com --recv-keys "C8DBA8065F261033D78A3AD9B2BFA790AECB1FF8"
+gpg --keyserver keyserver.ubuntu.com --recv-keys "$GPG_KEY_ID"
 openssl aes-256-cbc \
     -K "$encrypted_c5bb60cb91b9_key" \
     -iv "$encrypted_c5bb60cb91b9_iv" \
@@ -14,22 +14,6 @@ openssl aes-256-cbc \
     -d
 gpg --import sec.gpg
 
-echo "Setup publishing"
+echo "Publish release"
 
-cat <<-EOF > sonatype.sbt
-credentials in Global += Credentials(
-    "GnuPG Key ID",
-    "gpg",
-    "C8DBA8065F261033D78A3AD9B2BFA790AECB1FF8",
-    "ignored"
-)
-credentials in Global += Credentials(
-    "Sonatype Nexus Repository Manager",
-    "oss.sonatype.org",
-    "$SONATYPE_USERNAME",
-    "$SONATYPE_PASSWORD"
-)
-EOF
-
-# publish release
 sbt publish
