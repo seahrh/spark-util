92b191342662fbc6d3fab470b36b23ce6c6234af
seahrh <seahrh@gmail.com>
2019-04-23 09:56:45 +0800

    code comments

--- src/main/scala/com/sgcharts/sparkutil/Smote.scala
+++ src/main/scala/com/sgcharts/sparkutil/Smote.scala
@@ -11,6 +11,32 @@ import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
 
+/**
+  * Runs the SMOTE algorithm to generate synthetic examples of the minority class.
+  * This increases the size of the minority class, which is helpful for the imbalanced class problem.
+  *
+  * The synthetic examples are generated in the following way:
+  * First, randomly choose one of k neighbours for each example in the minority class.
+  * Then, take the difference of the feature vector between the sample and its neighbour.
+  * Multiply this difference by a random factor in the range [0, 1], and the result is the synthetic example.
+  *
+  * For discrete attributes, the synthetic example randomly picks either the original sample or the neighbour, and copies that value.
+  *
+  * The k-nearest neighbours are approximated by the Locality Sensitive Hashing (LSH) model in Spark ML.
+  *
+  * @see Chawla, N. V., Bowyer, K. W., Hall, L. O., & Kegelmeyer, W. P. (2002).
+  *      SMOTE: synthetic minority over-sampling technique. Journal of artificial intelligence research, 16, 321-357.
+  * @param sample                   pure minority class sample
+  * @param discreteStringAttributes names of discrete attributes with type [[String]]
+  * @param discreteLongAttributes   names of discrete attributes with type [[Long]]
+  * @param continuousAttributes     names of continuous attributes
+  * @param bucketLength             length of each bucket, parameter for [[org.apache.spark.ml.feature.BucketedRandomProjectionLSH]]
+  * @param numHashTables            number of hash tables, parameter for [[org.apache.spark.ml.feature.BucketedRandomProjectionLSH]]
+  * @param sizeMultiplier           number of synthetic examples to create, per example in the sample
+  * @param numNearestNeighbours     number of nearest neighbours to use for the creation of synthetic examples
+  * @param seed                     seed for random number generator
+  * @param spark                    SparkSession
+  */
 final case class Smote(
                         sample: Dataset[_],
                         discreteStringAttributes: Seq[String],
@@ -89,10 +115,16 @@ final case class Smote(
     .setOutputCol(featuresCol)
 
 
-  private val lsh: BucketedRandomProjectionLSH = new BucketedRandomProjectionLSH()
-    .setInputCol(featuresCol)
-    .setBucketLength(blen)
-    .setNumHashTables(numHashTables)
+  private val lsh: BucketedRandomProjectionLSH = {
+    val res = new BucketedRandomProjectionLSH()
+      .setInputCol(featuresCol)
+      .setBucketLength(blen)
+      .setNumHashTables(numHashTables)
+    seed match {
+      case Some(s) => res.setSeed(s)
+      case _ => res
+    }
+  }
 
   private val preprocessed: DataFrame = {
     val stages: Seq[PipelineStage] = stringIndexers ++ Seq(oneHotEncoder, assembler)
@@ -115,6 +147,14 @@ final case class Smote(
     res
   }
 
+  /**
+    * Uses LSH from Spark ML to approximate k-nearest neighbours for each example in the input sample
+    * (also known as the key).
+    *
+    * @example For a 2-column input, the resulting schema looks like: key_col1, key_col2, neighbours_col1 (list), neighbours_col2 (list)
+    * @param keyColumnPrefix prefix for the key column name
+    * @return k-nearest neighbours of each key
+    */
   private def nearestNeighbours(keyColumnPrefix: String): DataFrame = {
     val model: BucketedRandomProjectionLSHModel = lsh.fit(preprocessed)
     val lshDf: DataFrame = model.transform(preprocessed)
@@ -185,6 +225,16 @@ final case class Smote(
     new GenericRowWithSchema(vs.toArray, outSchema)
   }
 
+  /**
+    * Generates the synthetic examples as a DataFrame.
+    *
+    * The schema is exactly the union of the discreteStringAttributes,
+    * discreteLongAttributes and continuousAttributes that were passed in as constructor arguments.
+    *
+    * The size of the result should equal the size of the input sample multiplied by the sizeMultiplier.
+    *
+    * @return the synthetic examples
+    */
   def syntheticSample: DataFrame = {
     val keyColumnPrefix: String = "_smote_key_"
     val knn: DataFrame = nearestNeighbours(keyColumnPrefix)
