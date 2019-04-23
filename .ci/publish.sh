89aed82e6e4f7b0f0a08003e84ad02eba682217c
seahrh <seahrh@gmail.com>
2019-04-23 15:57:27 +0800

    do without sbt scripted plugin

---
+++ .ci/publish.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o verbose
+
+# Setup gpg keys
+gpg --keyserver keyserver.ubuntu.com --recv-keys "C8DBA8065F261033D78A3AD9B2BFA790AECB1FF8"
+openssl aes-256-cbc \
+    -K "$encrypted_c5bb60cb91b9_key" \
+    -iv "$encrypted_c5bb60cb91b9_iv" \
+    -in .ci/sec.gpg.enc \
+    -out sec.gpg \
+    -d
+gpg --import sec.gpg
+
+# Setup publishing
+cat <<-EOF > sonatype.sbt
+credentials in Global += Credentials(
+    "GnuPG Key ID",
+    "gpg",
+    "C8DBA8065F261033D78A3AD9B2BFA790AECB1FF8",
+    "ignored"
+)
+credentials in Global += Credentials(
+    "Sonatype Nexus Repository Manager",
+    "oss.sonatype.org",
+    "$SONATYPE_USERNAME",
+    "$SONATYPE_PASSWORD"
+)
+EOF
+
+# publish release
+sbt publish
